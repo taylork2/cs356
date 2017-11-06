c09a59f345bd02fb4eb9bc5adc9a7bbb341fd45e
Taylor Tu <tkt7@njit.edu>
2017-11-06 02:49:07 -0500

    Changed sent info, started bellman ford algo

--- hw3/router.py
+++ hw3/router.py
@@ -3,6 +3,7 @@ import sys
 import ctypes
 import struct 
 
+
 INF = 100000; #infinity 
 
 N = 4 #number of routers 
@@ -31,7 +32,7 @@ def createDistanceVector(router):
 			dists.append([INF, INF, INF, INF])
 
 	print "Router " + str(router) + "'s Distance vector"
-	print dists
+	printTable(dists)
 
 	return dists
 
@@ -39,15 +40,60 @@ def createMessage(seq_num, router, dists):
 	# convert to network byte order 
 	seq_ulong = socket.htonl(seq_num)
 	rou_ulong = socket.htonl(router)
-	dists_flat = [d for sublist in dists for d in sublist]
+	dists_flat = dists[router-1]
 
 	#message format (seq_num, router, flattened distance vector)
 	message = struct.pack('!ii%ui' %len(dists_flat), seq_ulong, rou_ulong, *dists_flat)
 	return message 
 
 def convertMsg(message):
-	d = struct.unpack('!ii16i', message)
-	seq_num = socket.nltoh(d[0]);  
+	m = struct.unpack('!ii4i', message)
+	seq_num = socket.ntohl(m[0])
+	router = socket.ntohl(m[1])
+	dists = m[2:]
+	return seq_num, router, dists 
+
+def updateDists(dists, sent_dists, sent_router):
+	updated = False
+	for j in range(0, len(sent_dists)):
+		if sent_dists[j] < dists[sent_router-1][j]:
+			dists[sent_router-1][j] = sent_dists[j]
+			updated = True
+
+	return dists, updated
+
+def bellmanFord(dists, router):
+	for j in range(0, len(dists)):
+		if j==router-1:
+			continue 
+
+		d, fh = D(dists,router-1,j)
+		dists[router-1][j] = d
+
+def D(dists, origin, dest):
+	min_d = INF
+	fh = -1
+	for j in range(0, len(dists)):
+		d = dists[origin][j] + D(dists, dest, i)[0]
+		if d < min_d:
+			min_d = d
+			fh = j
+
+	return min_d, fh
+
+
+def printTable(dists):
+	print "\t\tDest."
+	print "Src.\tR1\tR2\tR3\tR4"
+	row = 1
+	for l in dists:
+		sys.stdout.write("R"+str(row))
+		sys.stdout.flush()
+		row=row+1
+		for d in l:
+			sys.stdout.write("\t"+str(d)) 
+			sys.stdout.flush()
+		print
 
 
 # Check the commandline arguments
@@ -81,16 +127,20 @@ def main():
 		return 1
 
 	#set socket timeout of 1 second  
-	sock.settimeout(10)
+	sock.settimeout(1)
 
 	print >> sys.stderr, 'starting up on %s port %s' %serv_addr
 
-	seq_num = 1
+	seq_num = 0
 
-	try:
-		#Send distance vector to other routers 
+	#Send distance vector to other routers 
+	while True and seq_num<10: 
+
+		seq_num = seq_num + 1
+
 		for i in range(1, N+1):
-			if i==R:
+			# Skip itself and router's that are not neighbors 
+			if i==R or dists[R-1][i-1] >= INF:
 				continue
 
 			message = createMessage(seq_num, R, dists)
@@ -102,18 +152,24 @@ def main():
 			except socket.error as msg: 
 				print "Caught sendto exception: %s " %msg
 
+		for i in range(1, N+1):
 			try:
-				data, addr = sock.recvfrom(PORTS[i-1])
-				print "Receiving distance vector from router %s" %i
-				print data
+				data, addr = sock.recvfrom(24)
 			except socket.error as msg:
-				print "Caught recvfrom exception: %s " %msg
+				print "Caught recvfrom exception: %s " %msg	
+				continue
 
+		sent_seq_num, sent_router, sent_dists = convertMsg(data)
+		print "Received distance vector from router %s" %sent_router
+		dists, updated = updateDists(dists, sent_dists, sent_router)
+
+		if updated:
+			printTable(dists) 
 
 			
-	finally:
-		print "Closing socket" 
-		sock.close()	
+	
+	print "Closing socket" 
+	sock.close()	
 
 
 main()
